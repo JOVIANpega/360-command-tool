d1aa6d3162ec3f3457ddc29520ea7dbeba58975a
VALO360 <valo360@example.com>
2025-06-17 15:19:35 +0800

    更新版本號至1.36，更新檔案描述為'移動提醒視窗到最下方'

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
-echo 正在打包 VALO360指令通 V1.35...
+echo 正在打包 VALO360指令通 V1.36...
 pyinstaller --onefile --clean --noconfirm --icon=app.ico -w --version-file=version_info.txt --add-data "command.txt;." --add-data "user_guide.txt;." --add-data "setup.json;." --add-data "ui_parts;ui_parts" --name "VALO360指令通" main.py
 echo 打包完成，產生檔案位於 dist/VALO360指令通.exe
 pause 
